320fc75dcf27a81c6e6f2291270af6ff85fa9338
Alex Katsero <alexanderkatsero@gmail.com>
2020-06-07 13:39:34 +0300

    Replace argv with CONFIG

--- device/lib/IoT.js
+++ device/lib/IoT.js
@@ -1,7 +1,7 @@
 const { mqtt, auth, http, io, iot } = require('aws-crt');
 const { TextDecoder } = require('util');
 
-const args = {
+const CONFIG = {
 	endpoint: 'a24oab4599p0a-ats.iot.us-east-1.amazonaws.com',
 	key: 'keys/rpi-workshop-03-device-00.private.key',
 	cert: 'keys/rpi-workshop-03-device-00.cert.pem',
@@ -14,7 +14,7 @@ const args = {
 	verbosity: 'none'
 }
 
-async function execute_session(connection, argv) {
+async function execute_session(connection, CONFIG) {
 	return new Promise(async (resolve, reject) => {
 		try {
 			const decoder = new TextDecoder('utf8');
@@ -23,21 +23,21 @@ async function execute_session(connection, argv) {
 				console.log(`Publish received on topic ${topic}`);
 				console.log(json);
 				const message = JSON.parse(json);
-				if (message.sequence == argv.count) {
+				if (message.sequence == CONFIG.count) {
 					resolve();
 				}
 			}
 
-			await connection.subscribe(argv.topic, mqtt.QoS.AtLeastOnce, on_publish);
+			await connection.subscribe(CONFIG.topic, mqtt.QoS.AtLeastOnce, on_publish);
 
-			for (let op_idx = 0; op_idx < argv.count; ++op_idx) {
+			for (let op_idx = 0; op_idx < CONFIG.count; ++op_idx) {
 				const publish = async () => {
 					const msg = {
-						message: argv.message,
+						message: CONFIG.message,
 						sequence: op_idx + 1,
 					};
 					const json = JSON.stringify(msg);
-					connection.publish(argv.topic, json, mqtt.QoS.AtLeastOnce);
+					connection.publish(CONFIG.topic, json, mqtt.QoS.AtLeastOnce);
 				}
 				setTimeout(publish, op_idx * 1000);
 			}
@@ -48,23 +48,23 @@ async function execute_session(connection, argv) {
 	});
 }
 
-module.exports = async function main(argv = args) {
-	if (argv.verbosity != 'none') {
-		const level = parseInt(io.LogLevel[argv.verbosity.toUpperCase()]);
+module.exports = async function main() {
+	if (CONFIG.verbosity != 'none') {
+		const level = parseInt(io.LogLevel[CONFIG.verbosity.toUpperCase()]);
 		io.enable_logging(level);
 	}
 
 	const client_bootstrap = new io.ClientBootstrap();
 
-	const config_builder = iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(argv.cert, argv.key);
+	const config_builder = iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(CONFIG.cert, CONFIG.key);
 
-	if (argv.ca_file != null) {
-		config_builder.with_certificate_authority_from_path(undefined, argv.ca_file);
+	if (CONFIG.ca_file != null) {
+		config_builder.with_certificate_authority_from_path(undefined, CONFIG.ca_file);
 	}
 
 	config_builder.with_clean_session(false);
-	config_builder.with_client_id(argv.client_id);	
-	config_builder.with_endpoint(argv.endpoint);
+	config_builder.with_client_id(CONFIG.client_id);
+	config_builder.with_endpoint(CONFIG.endpoint);
 
 	// force node to wait 60 seconds before killing itself, promises do not keep node alive
 	const timer = setTimeout(() => {}, 60 * 1000);
@@ -74,7 +74,7 @@ module.exports = async function main(argv = args) {
 	const connection = client.new_connection(config);
 
 	await connection.connect()
-	await execute_session(connection, argv)
+	await execute_session(connection, CONFIG)
 
 	// Allow node to die if the promise above resolved
 	clearTimeout(timer);
