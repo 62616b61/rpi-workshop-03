20fc6ce58b0c91c6601dc18e7eefa5c372ae4149
Alex Katsero <alexanderkatsero@gmail.com>
2020-06-07 19:37:05 +0300

    Add API for publishing messages

---
+++ api/api.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+
+const { DEVICE_ENDPOINT, TOPIC } = process.env;
+const device = new AWS.IotData({ endpoint: DEVICE_ENDPOINT });
+
+module.exports.publish = async event => {
+  const body = JSON.parse(event.body)
+
+  await device.publish({
+    topic: TOPIC,
+    payload: JSON.stringify({ message: body.message }),
+    qos: 0,
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: 'ok',
+  };
+};
