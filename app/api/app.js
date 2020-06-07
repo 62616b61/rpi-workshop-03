7f9bf1ef02e843426b3e82ee1a1481c666cae96a
Alex Katsero <alexanderkatsero@gmail.com>
2020-06-07 20:21:24 +0300

    Remove publish function

--- app/api/app.js
+++ app/api/app.js
@@ -1,6 +1,31 @@
-module.exports.get = async () => {
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+const { DEVICE_ENDPOINT, TOPIC } = process.env;
+
+const publish = async ({ message }) => {
+  const device = new AWS.IotData({ endpoint: DEVICE_ENDPOINT });
+
+  return device.publish({
+    topic: TOPIC,
+    payload: JSON.stringify({ message }),
+    qos: 0,
+  }).promise();
+}
+
+module.exports.get = async (event) => {
+  const data = event.queryStringParameters || {};
+
+  if (data.message) {
+    await publish(data);
+  }
+
+  const html = fs.readFileSync(process.cwd() + '/views/app.html', 'utf8');
   return {
     statusCode: 200,
-    body: 'ok',
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,
   };
 };
